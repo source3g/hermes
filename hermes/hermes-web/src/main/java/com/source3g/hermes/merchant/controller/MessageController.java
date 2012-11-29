f58e7d26e618d15d02a3ab4c4dd86727fd9412ab
bin.zhao <zhaobin@source3g.com>
2012-11-29 17:17:02 +0800

    冲突

--- hermes/hermes-web/src/main/java/com/source3g/hermes/merchant/controller/MessageController.java
+++ hermes/hermes-web/src/main/java/com/source3g/hermes/merchant/controller/MessageController.java
@@ -3,9 +3,24 @@ package com.source3g.hermes.merchant.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.source3g.hermes.entity.merchant.Merchant;
@@ -13,11 +28,19 @@ import com.source3g.hermes.entity.message.MessageTemplate;
 import com.source3g.hermes.utils.ConfigParams;
 import com.source3g.hermes.utils.LoginUtils;
 
+import com.source3g.hermes.entity.merchant.Merchant;
+import com.source3g.hermes.utils.ConfigParams;
+import com.source3g.hermes.utils.LoginUtils;
+import com.source3g.hermes.utils.Page;
+
 @Controller
 @RequestMapping(value = "/merchant/message")
 public class MessageController {
 	
-
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@RequestMapping(value = "/template", method = RequestMethod.GET)
 	public ModelAndView template() {
 		
@@ -32,6 +55,20 @@ public class MessageController {
 		String uri=ConfigParams.getBaseUrl()+"shortMessage/template/add";
 		
 		return new ModelAndView("/merchant/shortMessage/messageTemplate");
+	}
+	
+	@RequestMapping(value = "/reservedMsgLog", method = RequestMethod.GET)
+	public ModelAndView reservedMsgLog(HttpServletRequest req,String pageNo) throws Exception {
+			Merchant merchant = LoginUtils.getLoginMerchant(req);	
+			Map<String, Object> model = new HashMap<String, Object>();	
+			if (StringUtils.isEmpty(pageNo)) {
+				pageNo = "1";
+			}
+			String uriMsgLog = ConfigParams.getBaseUrl() + "merchant/msgLogList/?pageNo=" + pageNo;
+			Page page = restTemplate.getForObject(uriMsgLog, Page.class);
+			model.put("page", page);
+			model.put("merchant", merchant);
+			return new ModelAndView("admin/shortMessage/reservedMessageLog",model);
 	}
 
 }
