a2f0eb324a4e5836be54ef7ab771c4a50258581e
bin.zhao <zhaobin@source3g.com>
2013-01-09 18:05:03 +0800

    fds

--- hermes/hermes-common/src/main/java/com/source3g/hermes/entity/customer/CustomerForSyncIntf.java
+++ hermes/hermes-common/src/main/java/com/source3g/hermes/entity/customer/CustomerForSyncIntf.java
@@ -2,7 +2,7 @@ package com.source3g.hermes.entity.customer;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(value = { "otherPhones", "operateTime" })
+@JsonIgnoreProperties(value = { "otherPhones", "operateTime","callRecords" })
 public interface CustomerForSyncIntf {
 
 }
