346f967b3b1eae702bdc3e8d30d0c185c527f243
chenjie <chenjie19881014@126.com>
2013-05-09 10:24:09 +0800

    ElectricMenu 增删查改

--- hermes/hermes-web/src/main/java/com/source3g/hermes/admin/controller/VersionController.java
+++ hermes/hermes-web/src/main/java/com/source3g/hermes/admin/controller/VersionController.java
@@ -20,7 +20,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
